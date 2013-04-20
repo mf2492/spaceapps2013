c01f0782e9da8a1800a9634ad409d2d70d108343
Brooks Swinnerton <bswinnerton@gmail.com>
2013-04-20 13:00:59 -0400

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Earthday::Application.config.secret_token = 'b6420f0a7e396a292f99169a9fed33f4e58fe0299be4d3fc6d181e8b5b3bc7b69b1543c54fa0583ca28b6702a0ab76464750466bb54f1015cea757fa040dbf24'
